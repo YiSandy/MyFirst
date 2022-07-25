d1752eff4601702ee44d9f59d89bf0e4350e6eb8
超级多笔记 <1061948884@qq.com>
2022-07-25 23:06:01 +0800

    first_commit

---
+++ src/test/java/com/nowcoder/demo/AppTest.java
@@ -0,0 +1,21 @@
+package com.nowcoder.demo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+	//注释
+        assertTrue( true );
+    }
+}
